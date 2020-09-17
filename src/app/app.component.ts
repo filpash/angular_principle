4865013c1940e911ef760129f14421c5c28b84d3
Filipp <fili92@mail.ru>
2020-09-17 16:17:34 +0300

    change parameters in components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string,
+  text: string,
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  posts: Post[] = [
+    {title: 'I want to learn Angular components', text: 'I have learnt components yet', id: 1},
+    {title: 'Second block will be about pipes', text: 'I will learn pipe soon', id: 2}
+  ]
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post)
+    console.log('Post ', post)
+  }
 }
